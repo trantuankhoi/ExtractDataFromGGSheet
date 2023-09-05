e9e02f1370e05d2b6597dac4f77f0e47a48dc781
KhoiTT <khoitt@KhoiTTs-MacBook-Pro.local>
2023-09-05 17:08:47 +0700

    update: update README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Extract data from Google Sheet
+## 🔍 Printed Text OCR
+## 🖋️ Author: KhoiTT
+
+### 🛠️ Install
+
+- Run command: `pip3 install -r requirements.txt`
+
+[Get GG API credential](https://developers.google.com/drive/api/quickstart/python)
+
+### 🧨 Using
+- Update file `config.py`
+- Run command `python3 export_data.py`
+
--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 CREDENTIAL_PATH = "/Users/KhoiTT/FTECH/02_Datasets/Tools/ExtractData/client_secret_567159421126-ho30t3hkj6i8s9ak8c6jdhf9hjj0suel.apps.googleusercontent.com.json"
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-SHEET_DIR = "/Users/KhoiTT/FTECH/02_Datasets/Tools/ExtractData/sheets"
-FOLDER_IDS = ["1C71jFb04hf56z5yM8r7PLMw2vgyW9mfN"]
+SHEET_DIR = "/Users/KhoiTT/FTECH/02_Datasets/Tools/ExtractData/sheets" # folder chứa các file sheets cần extract 
+FOLDER_IDS = ["1C71jFb04hf56z5yM8r7PLMw2vgyW9mfN"] # folder id lấy trong đường dẫn của folder trên drive
 
-COLUMN_INSTANCE_ID = 0
+COLUMN_INSTANCE_ID = 0 
 COLUMN_IMG_ID = 2
 COLUMN_GT_ID = 4
 CONFUSE_COLUMN_ID = 7
 
-IMAGE_DIR = "/Users/KhoiTT/FTECH/02_Datasets/Tools/ExtractData/task_3"
-GT_NAME = "test.txt"
+IMAGE_DIR = "/Users/KhoiTT/FTECH/02_Datasets/Tools/ExtractData/task_3" # folder chứa ảnh instances đã được extract
+GT_NAME = "test.txt" # tên file ground truth, lưu trong IMAGE_DIR
